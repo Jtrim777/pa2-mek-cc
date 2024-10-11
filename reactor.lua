4dfe92e9d304e67f0a1c5778132debbdd080f265
Jake Trimble <jatrimble777@gmail.com>
2024-10-11 15:34:54 -0400

    init

---
+++ reactor.lua
@@ -0,0 +1,84 @@
+local regbus = require("regbus")
+
+local REGISTERS = {
+    fuelLevel = 0,
+    fuelCapacity = 0,
+    coolantLevel = 0,
+    coolantCapacity = 0,
+    wasteLevel = 0,
+    wasteCapacity = 0,
+    steamLevel = 0,
+    steamCapacity = 0,
+    temperature = 0,
+    active = false,
+    burnRate = 0,
+    fuelRate = 0
+}
+
+local Reactor = {}
+
+function Reactor:new(name, pfSide, dbHost) 
+    if not peripheral.hasType(pfSide, "fissionReactorLogicAdapter") then
+        error("Peripheral on side " .. pfSide .. " is not a fissionReactorLogicAdapter")
+    end
+
+    control = peripheral.wrap(pfSide)
+    db = regbus.init_bridge(dbHost)
+
+    for k, iv in ipairs(REGISTERS) do 
+        db.create_register(name .. "." .. k, iv, true)
+    end
+
+    db.create_register("fission_reactors", {}, true)
+
+    local registry = db.read("fission_reactors")
+    registry[name] = os.getComputerID()
+    db.write("fission_reactors", registry)
+
+    o = { 
+        name = name,
+        control = control,
+        db = db
+    }
+
+    setmetatable(o, self)
+    self.__index = self
+
+    return o
+end
+
+function Reactor:syncState(tick, interval)
+    state = {
+        fuelLevel = self.control.getFuel().amount,
+        fuelCapacity = self.control.getFuelCapacity(),
+
+        coolantLevel = self.control.getCoolant().amount,
+        coolantCapacity = self.control.getCoolantCapacity(),
+
+        wasteLevel = self.control.getWaste().amount,
+        wasteCapacity = self.control.getWasteCapacity(),
+
+        steamLevel = self.control.getHeatedCoolant().amount,
+        steamCapacity = self.control.getHeatedCoolantCapacity(),
+
+        active = reactor.getStatus(),
+        temperature = reactor.getTemperature() - 273,
+
+        burnRate = reactor.getBurnRate()
+    }
+    local fuelRate = 0
+    if self.last and interval then 
+        local lastLevel = self.last.fuelLevel 
+        local adj = active and (lastLevel + (state.burnRate * interval)) or lastLevel
+        fuelRate = (state.fuelLevel - adj) / interval
+    end
+    state.fuelRate = fuelRate
+
+    self.last = state
+
+    for k, _ in ipairs(REGISTERS) do
+        self.db.write(self.name .. "." .. k, state[k])
+    end
+end
+
+return { Reactor = Reactor }
---
+++ regbus.lua
@@ -0,0 +1,204 @@
+
+local regbus = {}
+
+function regbus.init_host(name) 
+    local host = {
+        name = name
+    }
+
+    local registers = {}
+    host.registers = registers
+
+    local artificialRegisters = {}
+    host.artificialRegisters = artificialRegisters
+
+    local hooks = {}
+    host.hooks = hooks
+
+    function host.read(rname)
+        if registers[rname] then
+            return registers[rname]
+        end
+
+        if artificialRegisters[rname] then
+            return artificialRegisters[rname]()
+        end
+
+        return nil
+    end
+    
+    function host.write(rname, value)    
+        local og = registers[rname]
+        registers[rname] = value
+    
+        return og
+    end
+
+    function host.setupDNS()
+        rednet.host("regbus", host.name)
+    end
+
+    function host.createRegister(rname, init)
+        if registers[rname] or artificialRegisters[rname] then
+            error("register already exists")
+        end
+
+        if init == nil then
+            error("cannot set register to nil")
+        end
+
+        registers[rname] = init
+    end
+
+    function host.createArtificialRegister(rname, gen)
+        if registers[rname] or artificialRegisters[rname] then
+            error("register already exists")
+        end
+
+        artificialRegisters[rname] = gen
+    end
+
+    function host.hook(rname, fn)
+        if hooks[rname] then
+            cch = hooks[rname]
+            function nhook(v, ov)
+                cch(v, ov)
+                fn(v, ov)
+            end
+            hooks[rname] = nhook
+        else
+            hooks[rname] = fn
+        end
+    end
+
+    function host.on_rcv(sender, message)
+        local response = { success = true }
+
+        if message.cmd == "READ" then 
+            if registers[message.register] then 
+                response.value = registers[message.register]
+            elseif artificialRegisters[message.register] then 
+                response.value = artificialRegisters[message.register]()
+                response.generated = true 
+            else
+                response.success = false
+                response.error = "No such register with name " .. message.register 
+            end
+        elseif message.cmd == "WRITE" then 
+            if registers[message.register] then
+                if message.value == nil then 
+                    response.success = false
+                    response.error = "Cannot write nil to a register"
+                else 
+                    response.value = host.write(message.register, message.value)
+                    
+                    if hooks[message.register] then 
+                        hooks[message.register](message.value, response.value)
+                    end
+                end
+            else
+                response.success = false
+                response.error = "No writable register with name " .. message.register 
+            end
+        elseif message.cmd == "NEW" then
+            if registers[message.register] or artificialRegisters[message.register] then 
+                response.success = false 
+                response.error = "Register with that name already exists"
+            elseif message.value == nil then
+                response.success = false 
+                response.error = "Missing initial value for register"
+            else 
+                host.createRegister(message.register, message.value)
+            end
+        else 
+            response.success = false
+            response.error = "No such regbus command" .. message.cmd 
+        end 
+            
+        rednet.send(sender, response, "regbus")
+    end
+
+    function host.listen()
+        while true do
+            local sender, message = rednet.receive("regbus")
+
+            host.on_rcv(sender, message)
+        end
+    end
+
+    return host
+end
+
+function regbus.init_bridge(hostname) 
+    local bridge = {
+        hostname = hostname
+    }
+
+    local cid = rednet.lookup("regbus", hostname)
+
+    if not cid then 
+        error("No registered regbus host with name " .. hostname)
+    end 
+
+    bridge.hostId = cid
+
+    function bridge.read(rname)    
+        rednet.send(bridge.hostId, { cmd = "READ", register = rname }, "regbus")
+    
+        local _, response = rednet.receive("regbus", 5)
+    
+        if not response then
+            error("regbus host did not reply")
+        end
+    
+        if not response.success then 
+            error("regbus cmd failed with error: " .. response.error)
+        end
+    
+        return response.value 
+    end
+    
+    function bridge.write(rname, value, faf)
+        rednet.send(bridge.hostId, { cmd = "WRITE", register = rname, value = value }, "regbus")
+    
+        if faf then
+            return nil
+        end
+    
+        local _, response = rednet.receive("regbus", 5)
+    
+        if not response then
+            error("regbus host did not reply")
+        end
+    
+        if not response.success then 
+            error("regbus cmd failed with error: " .. response.error)
+        end
+    
+        return response.value 
+    end
+
+    function bridge.create_register(rname, init, faf)
+        rednet.send(bridge.hostId, { cmd = "NEW", register = rname, value = init }, "regbus")
+    
+        if faf then
+            return nil
+        end
+    
+        local _, response = rednet.receive("regbus", 5)
+    
+        if not response then
+            error("regbus host did not reply")
+        end
+    
+        if not response.success then 
+            error("regbus cmd failed with error: " .. response.error)
+        end
+    
+        return response.value 
+    end
+
+    return bridge
+end
+
+return regbus
