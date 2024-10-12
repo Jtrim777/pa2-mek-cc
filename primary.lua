d3f26b971d8329b6eb8af6a78402c3470a636b57
Jake Trimble <jatrimble777@gmail.com>
2024-10-12 01:28:17 -0400

    update

--- primary.lua
+++ primary.lua
@@ -14,22 +14,24 @@ function updateDisplay()
     matrixSourceRate = matrix.getLastInput()
     matrixSinkRate = matrix.getLastOutput()
 
+    screen.setBackgroundColor(colors.black)
     screen.clear()
     screen.setCursorPos(1, 1)
-    screen.setBackgroundColor(colors.white)
 
-    screen.setTextColor(colors.black)
+    screen.setTextColor(colors.white)
     screen.write("Main Reactor is ")
     screen.setTextColor(reactorOn and colors.green or colors.red)
     screen.write(reactorOn and "ON  " or "OFF ")
-    screen.setTextColor(colors.black)
+    screen.setTextColor(colors.white)
 
     screen.setCursorPos(1, 2)
     screen.write("Fuel ")
 
     fuelBoxes = reactorFuelPercent * 10
     paintutils.drawFilledBox(6, 2, 6 + fuelBoxes, 2, colors.orange)
+    screen.setBackgroundColor(colors.black)
     paintutils.drawBox(6 + fuelBoxes + 1, 2, 16, 2, colors.orange)
+    screen.setBackgroundColor(colors.black)
     screen.setCursorPos(17, 2)
     screen.write(("%.2f B"):format(reactorFuelLevel/1000.0))
 end
